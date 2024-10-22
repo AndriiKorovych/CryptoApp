6c84597aa2446b446f4f22307a8cce228eb61ac6
mooneiko <moon@gmail.com+>
2024-10-22 23:09:56 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace CryptoApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnEncryptAES;
+        private System.Windows.Forms.Button btnDecryptAES;
+        private System.Windows.Forms.Button btnEncryptRSA;
+        private System.Windows.Forms.Button btnDecryptRSA;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label lblFileName;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnEncryptAES = new System.Windows.Forms.Button();
+            this.btnDecryptAES = new System.Windows.Forms.Button();
+            this.btnEncryptRSA = new System.Windows.Forms.Button();
+            this.btnDecryptRSA = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(20, 20);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(70, 17);
+            this.lblFileName.TabIndex = 0;
+            this.lblFileName.Text = "File Name";
+
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(100, 20);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(250, 22);
+            this.txtFileName.TabIndex = 1;
+
+            // 
+            // btnEncryptAES
+            // 
+            this.btnEncryptAES.Location = new System.Drawing.Point(100, 60);
+            this.btnEncryptAES.Name = "btnEncryptAES";
+            this.btnEncryptAES.Size = new System.Drawing.Size(100, 30);
+            this.btnEncryptAES.TabIndex = 2;
+            this.btnEncryptAES.Text = "Encrypt AES";
+            this.btnEncryptAES.UseVisualStyleBackColor = true;
+            this.btnEncryptAES.Click += new System.EventHandler(this.btnEncryptAES_Click);
+
+            // 
+            // btnDecryptAES
+            // 
+            this.btnDecryptAES.Location = new System.Drawing.Point(250, 60);
+            this.btnDecryptAES.Name = "btnDecryptAES";
+            this.btnDecryptAES.Size = new System.Drawing.Size(100, 30);
+            this.btnDecryptAES.TabIndex = 3;
+            this.btnDecryptAES.Text = "Decrypt AES";
+            this.btnDecryptAES.UseVisualStyleBackColor = true;
+            this.btnDecryptAES.Click += new System.EventHandler(this.btnDecryptAES_Click);
+
+            // 
+            // btnEncryptRSA
+            // 
+            this.btnEncryptRSA.Location = new System.Drawing.Point(100, 100);
+            this.btnEncryptRSA.Name = "btnEncryptRSA";
+            this.btnEncryptRSA.Size = new System.Drawing.Size(100, 30);
+            this.btnEncryptRSA.TabIndex = 4;
+            this.btnEncryptRSA.Text = "Encrypt RSA";
+            this.btnEncryptRSA.UseVisualStyleBackColor = true;
+            this.btnEncryptRSA.Click += new System.EventHandler(this.btnEncryptRSA_Click);
+
+            // 
+            // btnDecryptRSA
+            // 
+            this.btnDecryptRSA.Location = new System.Drawing.Point(250, 100);
+            this.btnDecryptRSA.Name = "btnDecryptRSA";
+            this.btnDecryptRSA.Size = new System.Drawing.Size(100, 30);
+            this.btnDecryptRSA.TabIndex = 5;
+            this.btnDecryptRSA.Text = "Decrypt RSA";
+            this.btnDecryptRSA.UseVisualStyleBackColor = true;
+            this.btnDecryptRSA.Click += new System.EventHandler(this.btnDecryptRSA_Click);
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(400, 150);
+            this.Controls.Add(this.btnEncryptAES);
+            this.Controls.Add(this.btnDecryptAES);
+            this.Controls.Add(this.btnEncryptRSA);
+            this.Controls.Add(this.btnDecryptRSA);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.lblFileName);
+            this.Name = "Form1";
+            this.Text = "CryptoApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CryptoApp
+{
+    public partial class Form1 : Form
+    {
+        private byte[] aesKey;
+        private byte[] aesIV;
+        private RSACryptoServiceProvider rsa;
+
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateAESKeyAndIV();
+            rsa = new RSACryptoServiceProvider(2048); // Генерація ключів RSA
+        }
+
+        // Генерація AES ключа та IV
+        private void GenerateAESKeyAndIV()
+        {
+            aesKey = GenerateRandomBytes(32); // 256-бітний ключ
+            aesIV = GenerateRandomBytes(16);  // 128-бітний IV
+        }
+
+        private byte[] GenerateRandomBytes(int length)
+        {
+            byte[] bytes = new byte[length];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(bytes);
+            }
+            return bytes;
+        }
+
+        // Шифрування файлу з AES
+        private void btnEncryptAES_Click(object sender, EventArgs e)
+        {
+            string inputFile = txtFileName.Text + ".txt";
+            string encryptedFile = txtFileName.Text + ".aes";
+
+            if (File.Exists(inputFile))
+            {
+                EncryptAES(inputFile, encryptedFile);
+                MessageBox.Show("Файл зашифровано успішно!");
+            }
+            else
+            {
+                MessageBox.Show("Файл не знайдено!");
+            }
+        }
+
+        private void EncryptAES(string inputFile, string encryptedFile)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = aesKey;
+                aes.IV = aesIV;
+
+                using (var encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
+                using (var inputStream = File.OpenRead(inputFile))
+                using (var outputStream = File.OpenWrite(encryptedFile))
+                using (var cryptoStream = new CryptoStream(outputStream, encryptor, CryptoStreamMode.Write))
+                {
+                    inputStream.CopyTo(cryptoStream);
+                }
+            }
+        }
+
+        // Дешифрування файлу з AES
+        private void btnDecryptAES_Click(object sender, EventArgs e)
+        {
+            string encryptedFile = txtFileName.Text + ".aes";
+            string decryptedFile = txtFileName.Text + "_decrypted.txt";
+
+            if (File.Exists(encryptedFile))
+            {
+                DecryptAES(encryptedFile, decryptedFile);
+                MessageBox.Show("Файл розшифровано успішно!");
+            }
+            else
+            {
+                MessageBox.Show("Зашифрований файл не знайдено!");
+            }
+        }
+
+        private void DecryptAES(string encryptedFile, string decryptedFile)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = aesKey;
+                aes.IV = aesIV;
+
+                using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+                using (var inputStream = File.OpenRead(encryptedFile))
+                using (var outputStream = File.OpenWrite(decryptedFile))
+                using (var cryptoStream = new CryptoStream(inputStream, decryptor, CryptoStreamMode.Read))
+                {
+                    cryptoStream.CopyTo(outputStream);
+                }
+            }
+        }
+
+        // Шифрування файлу з RSA
+        private void btnEncryptRSA_Click(object sender, EventArgs e)
+        {
+            string inputFile = txtFileName.Text + ".txt";
+            string encryptedFile = txtFileName.Text + ".rsa";
+
+            if (File.Exists(inputFile))
+            {
+                byte[] data = File.ReadAllBytes(inputFile);
+                byte[] encryptedData = rsa.Encrypt(data, false);
+                File.WriteAllBytes(encryptedFile, encryptedData);
+                MessageBox.Show("Файл зашифровано RSA!");
+            }
+            else
+            {
+                MessageBox.Show("Файл не знайдено!");
+            }
+        }
+
+        // Дешифрування файлу з RSA
+        private void btnDecryptRSA_Click(object sender, EventArgs e)
+        {
+            string encryptedFile = txtFileName.Text + ".rsa";
+            string decryptedFile = txtFileName.Text + "_decrypted.txt";
+
+            if (File.Exists(encryptedFile))
+            {
+                byte[] encryptedData = File.ReadAllBytes(encryptedFile);
+                byte[] decryptedData = rsa.Decrypt(encryptedData, false);
+                File.WriteAllBytes(decryptedFile, decryptedData);
+                MessageBox.Show("Файл розшифровано RSA!");
+            }
+            else
+            {
+                MessageBox.Show("Зашифрований файл не знайдено!");
+            }
+        }
+    }
+}
